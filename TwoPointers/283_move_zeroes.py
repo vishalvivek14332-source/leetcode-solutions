e8453dd4a665107ea62d29782f816d73e0950c11
Vishal Vivek <vishalvivek14332@gmail.com>
2026-06-30 22:11:41 +0530

    Solve LeetCode 283 Move Zeroes

---
+++ TwoPointers/283_move_zeroes.py
@@ -0,0 +1,10 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        i=0
+        j=0
+        l=len(nums)
+        while i<l:
+            if nums[i]!=0:
+                nums[i],nums[j] = nums[j],nums[i]
+                j+=1
+            i+=1
